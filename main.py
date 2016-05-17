2a8f55097a29f2bd32555fabe268a3cd26c318d9
gokulvasan <gokulvas@gmail.com>
2016-05-17 04:05:25 +0200

    TIME TO RUN PATCH

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from simso.core import Model
 from simso.configuration import Configuration
 from interval import *
 from ss_conf_table import *
-
+#from ss_offline_phase import gen_slotshift_table
 
 def main():
 
@@ -12,16 +12,23 @@ def main():
 	configuration = Configuration()
 
 	if remote == None:
+		print "remote is none"
 		#todo: this number should be generated randomly
 		table = gen_slotshift_table(1, 10, 10, 10, .10)
-		remote = local_table(table[0], table[1],
+		if table:
+			remote = local_table(table[0], table[1],
 				table[2], table[3])
+		else:
+			print "Cant Run because task set not generated"
+			return
 	else:
 		remote.recieve_table()
 
 	associate = association(remote)
 
 	interval = associate.create_def_intr_list(associate)
+	
+	run_time = associate.get_running_time(interval)
 
 	associate.create_relation_window(associate, interval)
 
@@ -30,7 +37,7 @@ def main():
 		if(j != 0):
 			break
 
-	configuration.duration = 420 * configuration.cycles_per_ms
+	configuration.duration = run_time * configuration.cycles_per_ms
 
 	configuration.add_processor(name="CPU 1", identifier=1)
 
